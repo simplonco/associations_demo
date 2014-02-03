ed671db6d6b5e60916e1a3f60acc956b41472ed9
MickyWillys <fozeum@yahoo.fr>
2014-02-03 11:22:59 +0100

    initial commit

---
+++ associations_demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AssociationsDemo::Application.config.secret_key_base = '638092da9decbc0d79b3303881b20c8ce15cf9b847f96d442847b3b9c7ef75f8297a88ea0fa9ad9f7da6901ae37bfdf4c610f433528bdbc5965f8d148a499eed'
---
+++ associations_demo/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AssociationsDemo::Application.config.session_store :cookie_store, key: '_associations_demo_session'
