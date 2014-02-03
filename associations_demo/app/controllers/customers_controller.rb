706737d25ccc71bb249d9b92a19880092050cadd
MickyWillys <fozeum@yahoo.fr>
2014-02-03 15:47:25 +0100

    hasmany done

--- associations_demo/app/controllers/customers_controller.rb
+++ associations_demo/app/controllers/customers_controller.rb
@@ -2,7 +2,6 @@ class CustomersController < ApplicationController
 def show
 	@customer= Customer.find(params[:id])
 	@orders= Order.where(customer_id: @customer.id) 
-	# binding.pry
 end 
 
 end
--- associations_demo/app/controllers/orders_controller.rb
+++ associations_demo/app/controllers/orders_controller.rb
@@ -1,3 +1,6 @@
 class OrdersController < ApplicationController
-
+	def show
+		@order= Order.find(params[:id])
+		@customer = Customer.find(@order.customer_id)
+	end
 end
